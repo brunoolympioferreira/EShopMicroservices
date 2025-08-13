02c87f009a7bca4470f63cac69998f3b5e940cfb
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-08-13 10:38:41 -0300

    Implementa Base de Dados via SQLite

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,6 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
     app.MapGrpcReflectionService();
 
 // Configure the HTTP request pipeline.
+app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
