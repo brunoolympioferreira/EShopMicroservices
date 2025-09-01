f5652faf642cf33959164cebb2a411ba5dd6fd35
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-09-01 13:09:45 -0300

    Aplicando DDD em ValueObjects

---
+++ src/Services/Ordering/Ordering.Domain/Models/Custumer.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Domain.Models;
+public class Custumer : Entity<CustomerId>
+{
+    public string Name { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
+
+    public static Custumer Create(CustomerId id, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+        var custumer = new Custumer
+        {
+            Id = id,
+            Name = name,
+            Email = email
+        };
+
+        return custumer;
+    }
+}
