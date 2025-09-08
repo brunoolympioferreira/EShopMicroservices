a3750b9184e606de972ee2a4f4a22b17c9eb8450
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-09-08 18:20:55 -0300

    Implementa DbContext

---
+++ src/Services/Ordering/Ordering.Infrasctructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System.Reflection;
+
+namespace Ordering.Infrasctructure.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base(options) { }
+
+    public DbSet<Custumer> Custumers => Set<Custumer>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
