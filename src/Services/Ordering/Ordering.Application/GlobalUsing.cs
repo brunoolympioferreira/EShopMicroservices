1eaa3f640e11bc69eccaa7734eacef582acd73b7
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-09-22 17:01:44 -0300

    UpdateOrder e CreateOrder

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -2,3 +2,7 @@
 global using BuildingBlocks.CQRS;
 global using FluentValidation;
 global using Ordering.Application.Dtos;
+global using Ordering.Application.Data;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Application.Exceptions;
